a6dc972245f777b98fb79f56721130cfd393e8ac
christian iverson <chris.w.iverson@gmail.com>
2020-12-15 13:12:52 -0800

    commit tree b1aa7703db9b7e2a644eddf8a8af23079e7d23d8

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+require "pry"
+class Anagram
+
+    def initialize(word)
+        @word=word
+        
+    end
+    def match(array)
+        result=[]
+        array.each {|element|
+            compare = element.split("").sort
+            
+           if @word.split("").sort==compare
+            
+            result.push(element)
+           end
+        }
+        result
+        
+    end
+end
